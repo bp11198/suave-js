5b908d5104da6fb0f64ab740c3bcac2bacec4ad2
bp11198 <bp11198>
2023-08-21 23:23:05 +0700

    feature expedia

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+const fillOutPaymentForm = (cardNumber, expirationDate, cardholderName, securityCode) => {
 //set card number
-let elsCardNumber = document.getElementsByName('creditCards[0].card_number');
-let i;
-for (i = 0; i < elsCardNumber.length; i++)
-    elsCardNumber[i].value = "card_number";
+    let elsCardNumber = document.getElementsByName('creditCards[0].card_number');
+    let i;
+    for (i = 0; i < elsCardNumber.length; i++)
+        elsCardNumber[i].value = cardNumber;
 
 //set month and year expiration
-let elsExpiration = document.getElementsByName('creditCards[0].expiration_date');
-for (i = 0; i < elsExpiration.length; i++)
-    elsExpiration[i].value = "expiration_date";
+    let elsExpiration = document.getElementsByName('creditCards[0].expiration_date');
+    for (i = 0; i < elsExpiration.length; i++)
+        elsExpiration[i].value = expirationDate;
 
 //set month and year expiration
-let elsNameOnCard = document.getElementsByName('creditCards[0].cardholder_name');
-for (i = 0; i < elsNameOnCard.length; i++)
-    elsNameOnCard[i].value = "cardholder_name";
+    let elsNameOnCard = document.getElementsByName('creditCards[0].cardholder_name');
+    for (i = 0; i < elsNameOnCard.length; i++)
+        elsNameOnCard[i].value = cardholderName;
 
 //set CVC
-let elsCVC = document.getElementsByName('creditCards[0].new_card_security_code');
-for (i = 0; i < elsCVC.length; i++)
-    elsCVC [i].value = "new_card_security_code";
+    let elsCVC = document.getElementsByName('creditCards[0].new_card_security_code');
+    for (i = 0; i < elsCVC.length; i++)
+        elsCVC[i].value = securityCode;
+}
